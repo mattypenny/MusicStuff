72461a4948402a0e5204538ce1e6486eac72375d
mattypenny_on_lulu <mattypenny@gmail.com>
2025-10-01 18:01:52 +0100

    first more-or-less working version

---
+++ functions/Get-MsSongsFromDiscogs.ps1
@@ -0,0 +1,46 @@
+function Get-MsSongsFromDiscogs {
+    <#
+.SYNOPSIS
+   xx
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True)][string]$DiscogsCode
+    )
+   
+    $DebugPreference = $PSCmdlet.GetVariableValue('DebugPreference')
+   
+    write-startfunction
+   
+    $uri = "https://api.discogs.com/releases/$DiscogsCode"
+
+    try {
+        $Response = invoke-restmethod $Uri -ErrorAction Stop
+    
+    }
+    catch {
+        throw "Error retrieving data from Discogs API for release code <$DiscogsCode>"
+    }
+   
+    $LpTitle = $Response.title
+    $Released = $Response.released
+
+    $TrackList = $Response   | select -ExpandProperty tracklist 
+
+    foreach ($Track in $TrackList) {
+        $ArtistSong = "$($Track.artists[0].name) - $($Track.title)"
+        $Artist = $Track.artists[0].name
+        $Song = $Track.title
+       
+        [PSCustomObject]@{
+            ArtistSong = $ArtistSong
+            Artist     = $Artist
+            Song       = $Song
+            LpTitle    = $LpTitle
+            Released   = $Released
+        }
+    }
+    write-endfunction
+   
+   
+}
